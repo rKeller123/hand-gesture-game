c87f19d483feafc39cc4cb632e6dc55c9f608db7
Jolan Knüsel <jolan.knuesel@students.unibe.ch>
2025-11-20 17:50:42 +0100

    Refactored logic into Game and visuals into Gamescreen. Added next question logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import './App.css'
 import { GestureDetectionProvider } from './common/gesture_detection/GestureDetectionContext'
 import Fingerpose_Poc from './components/fingerpose_poc'
 import Gamescreen from "./components/gamescreen/Gamescreen.jsx";
+import Game from "./components/game";
 
 function App() {
 
   return (
     <GestureDetectionProvider>
-      <Gamescreen />
-      <Fingerpose_Poc />
+      <Game />
+        <Fingerpose_Poc/>
     </GestureDetectionProvider>
   )
 }
