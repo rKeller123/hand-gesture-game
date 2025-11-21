cf53eb7b5dab5706641354e8b97443e25c1d7ff8
KJol21 <jolan.knuesel@students.unibe.ch>
2025-11-21 14:19:07 +0100

    Merge pull request #1
    * First version of a somewhat functional gamescreen
    
    * Added pose detection for selecting answer
    
    * Refactored logic into Game and visuals into Gamescreen. Added next qu…
    
    * Added progress bar when a gesture is detected
    
    * slightly improved visuals
    
    * removed unused pictures

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { GestureDetectionProvider } from './common/gesture_detection/GestureDetectionContext'
 import Fingerpose_Poc from './components/fingerpose_poc'
+import Gamescreen from "./components/gamescreen/Gamescreen.jsx";
+import Game from "./components/game";
 
 function App() {
 
   return (
     <GestureDetectionProvider>
-      <Fingerpose_Poc />
+      <Game />
     </GestureDetectionProvider>
   )
 }
